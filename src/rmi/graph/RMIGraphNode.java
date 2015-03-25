ee7476a1dda32919655cddd3c2ddac9f89c2695a
Julien <brabant.julien@gmail.com>
2015-03-25 23:12:46 +0100

    Add GraphNode

--- src/rmi/graph/RMIGraphNode.java
+++ src/rmi/graph/RMIGraphNode.java
@@ -1,5 +1,28 @@
 package rmi.graph;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
 
-public interface RMIGraphNode extends Remote {}
+public interface RMIGraphNode extends Remote {
+
+	public void addNeighbor(RMIGraphNode neighbor) throws RemoteException;
+
+	public void removeNeighbor(int index) throws RemoteException;
+
+	public void removeNeighbor(RMIGraphNode neighbor) throws RemoteException;
+
+	public void clearNeighbors() throws RemoteException;
+
+	public List<RMIGraphNode> getNeighbors() throws RemoteException;
+
+	public RMIGraphNode getNeighbor(int index) throws RemoteException;
+	
+	public String getName() throws RemoteException;
+	
+	public String getTrace() throws RemoteException;
+	
+	public String propagate(byte[] data) throws RemoteException;
+	
+	public String sendDataToNeighbors(byte[] data) throws RemoteException;
+}
--- src/rmi/graph/RMIGraphNodeImpl.java
+++ src/rmi/graph/RMIGraphNodeImpl.java
@@ -1,3 +1,97 @@
 package rmi.graph;
 
-public class RMIGraphNodeImpl {}
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+
+public class RMIGraphNodeImpl extends UnicastRemoteObject implements RMIGraphNode{
+
+	private List<RMIGraphNode> neighbors;
+	private String name;
+	private String trace;
+
+	protected RMIGraphNodeImpl(String name, List<RMIGraphNode> neighbors) throws RemoteException {
+		this.name = name;
+		this.trace = "";
+		this.neighbors = neighbors;
+	}
+
+	@Override
+	public void addNeighbor(RMIGraphNode neighbor) throws RemoteException {
+		this.neighbors.add(neighbor);
+	}
+
+	@Override
+	public void removeNeighbor(int index) throws RemoteException {
+		this.neighbors.remove(index);
+	}
+
+	@Override
+	public void removeNeighbor(RMIGraphNode neighbor) throws RemoteException {
+		this.neighbors.remove(neighbor);
+	}
+
+	@Override
+	public void clearNeighbors() throws RemoteException {
+		this.neighbors.clear();
+	}
+
+	@Override
+	public List<RMIGraphNode> getNeighbors() throws RemoteException {
+		return this.neighbors;
+	}
+
+	@Override
+	public RMIGraphNode getNeighbor(int index) throws RemoteException {
+		return this.neighbors.get(index);
+	}
+
+	@Override
+	public String getName() throws RemoteException {
+		return this.name;
+	}
+
+	@Override
+	public String getTrace() throws RemoteException {
+		return this.trace;
+	}
+
+	@Override
+	public String propagate(byte[] data) throws RemoteException {
+		this.trace = name + " is propagating...\n";
+		trace += sendDataToNeighbors(data);
+		return trace;
+	}
+
+	@Override
+	public String sendDataToNeighbors(byte[] data) throws RemoteException {
+		this.trace = "";
+
+		if(this.neighbors.size() == 0) {
+			this.trace = "Data = " + new String(data) + "\n";
+			return trace;
+		}
+
+		for(int i = 0; i < this.neighbors.size(); i++) {
+			final int finalIndice = i;
+			final byte[] finalData = data;
+
+			Thread thread = new Thread() {				
+				public void run() {
+					try {
+						trace += "Sending to child " + neighbors.get(finalIndice).getName() + "\n";
+						trace += neighbors.get(finalIndice).propagate(finalData);
+					} catch (RemoteException e) {
+						System.out.println("Remote exception when sending data to neighbor " + finalIndice + " of " + name);
+					}						
+				}	
+
+			};
+
+			thread.run();
+		}
+
+		return trace;
+	}
+
+}
