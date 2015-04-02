d83db259671ab24f18b9e4738e3a664e8b7bc8e3
Cachera Nicolas <phoenix-left@hotmail.fr>
2015-04-02 10:57:37 +0200

    Add jars

--- src/rmi/client/RMIGraphNodeClient.java
+++ src/rmi/client/RMIGraphNodeClient.java
@@ -25,7 +25,7 @@ public class RMIGraphNodeClient {
 			System.exit(1);
 		}
 		
-		node = (RMIGraphNode)Naming.lookup(args [0]);
+		node = (RMIGraphNode)Naming.lookup(args[0]);
 		trace = node.propagate("42".getBytes(), path);
 		
 		System.out.println(trace);
