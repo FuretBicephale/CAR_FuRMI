f647bee55862f9ca7d66af8f7c580d0044d655d7
Cachera Nicolas <phoenix-left@hotmail.fr>
2015-04-02 10:50:21 +0200

    Add doc, .jar and use .jar in scripts

--- createGraph.sh
+++ createGraph.sh
@@ -8,23 +8,23 @@ rmiregistry_pid="$!"
 
 cd ..
 
-xterm -e "java -classpath bin rmi.server.RMIGraphNodeServer node1" &
-sleep 0.1
+xterm -e "java -jar RMIGraphNodeServer.jar node1" &
+sleep 1
 
-xterm -e "java -classpath bin rmi.server.RMIGraphNodeServer node2 node1" &
-sleep 0.1
+xterm -e "java -jar RMIGraphNodeServer.jar node2 node1" &
+sleep 1
 
-xterm -e "java -classpath bin rmi.server.RMIGraphNodeServer node3 node2 node1" &
-sleep 0.1
+xterm -e "java -jar RMIGraphNodeServer.jar node3 node2 node1" &
+sleep 1
 
-xterm -e "java -classpath bin rmi.server.RMIGraphNodeServer node4 node2" &
-sleep 0.1
+xterm -e "java -jar RMIGraphNodeServer.jar node4 node2" &
+sleep 1
 
-xterm -e "java -classpath bin rmi.server.RMIGraphNodeServer node5 node1" &
-sleep 0.1
+xterm -e "java -jar RMIGraphNodeServer.jar node5 node1" &
+sleep 1
 
-xterm -e "java -classpath bin rmi.server.RMIGraphNodeServer node6" &
-sleep 0.1
+xterm -e "java -jar RMIGraphNodeServer.jar node6" &
+sleep 1
 
 echo -n "Press any key to continue... "
 read var_end
--- createTree.sh
+++ createTree.sh
@@ -9,25 +9,25 @@ rmiregistry_pid="$!"
 cd ..
 
 # Root
-xterm -e "java -classpath bin rmi.server.RMITreeNodeServer root" &
-sleep 0.1
+xterm -e "java -jar RMITreeNodeServer.jar root" &
+sleep 1
 
 # 1er etage
-xterm -e "java -classpath bin rmi.server.RMITreeNodeServer node1 root" &
-sleep 0.1
+xterm -e "java -jar RMITreeNodeServer.jar node1 root" &
+sleep 1
 
-xterm -e "java -classpath bin rmi.server.RMITreeNodeServer node2 root" &
-sleep 0.1
+xterm -e "java -jar RMITreeNodeServer.jar node2 root" &
+sleep 1
 
 # 2eme etage
-xterm -e "java -classpath bin rmi.server.RMITreeNodeServer leaf1 node1" & 
-sleep 0.1
+xterm -e "java -jar RMITreeNodeServer.jar leaf1 node1" & 
+sleep 1
 
-xterm -e "java -classpath bin rmi.server.RMITreeNodeServer leaf2 node1" &
-sleep 0.1
+xterm -e "java -jar RMITreeNodeServer.jar leaf2 node1" &
+sleep 1
 
-xterm -e "java -classpath bin rmi.server.RMITreeNodeServer leaf3 node2" &
-sleep 0.1
+xterm -e "java -jar RMITreeNodeServer.jar leaf3 node2" &
+sleep 1
 
 echo -n "Press any key to continue... "
 read var_end
